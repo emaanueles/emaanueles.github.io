5498c95684511f07a8a9871f4ed9ad8b18a60a3d
emaanueles <131375151+emaanueles@users.noreply.github.com>
2024-08-28 10:30:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 import {aleatorio, nome} from './aleatorio.js';
-caixaPerguntas.textContent = `Em 2049, ${nome}`;
-function substituiNome(){
-    for(const pergunta of perguntas){
-        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
-    }
-}
-substituiNome();
+import {aleatorio, nome} from './perguntas.js';
+
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraResultado() {
+caixaPerguntas.textContent = "Em 2049, ...";  
+textoResultado.textContent = historiaFinal; 
+caixaAlternativas.textContent = ""; 
+caixaResultado.classList.add("mostrar");
+botaoJogarNovamente.addEventistener("click", jogarNovamente);
